2492231df47c1735c43d07a93006ef7a7e0db374
saddamsaggar400-oss <saddam.saggar400@icloud.com>
2026-09-01 03:56:48 +0300

    Remove internal backup and memory files

--- .migration-backup/next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  outputFileTracingRoot: path.resolve(__dirname),
-};
-
-module.exports = nextConfig;
